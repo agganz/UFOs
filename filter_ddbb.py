ec4e0b29469b47496f237b9a299a12ddfa097a31
Alejandro Gonzalez <agganzabal@gmail.com>
2024-01-18 10:30:58 +0100

    0.2 (AG): now the search can be done with pulses

--- filter_ddbb.py
+++ filter_ddbb.py
@@ -7,12 +7,13 @@ Created on Mon Nov 13 16:44:13 2023
 
 ChangeLog
 
-    V.- 0.1 (AG): First version (GIT)
+    0.1 (AG): First version (GIT)
+    0.2 (AG): Added pulse search; changed function arguments.
 """
 
 import pandas as pd
 
-def filter_camera_ddbb(camera_code):
+def filter_camera_ddbb(fieldname, expr):
     """
     Filters the database found in complete_UFO_ddbb.xlsx and returns the cases 
     for the given camera passed as argument.
@@ -30,9 +31,14 @@ def filter_camera_ddbb(camera_code):
 
     """
     
-    ufo_path = r"C:\Users\Doctorando1\Documents\UFOs\data\complete_UFO_ddbb.xlsx"
+    ufo_path = "./data/complete_UFO_ddbb.xlsx"
     UFO_df = pd.read_excel(ufo_path)
     
-    op_camera_rows = UFO_df[UFO_df['Camera'].str.contains(camera_code)]
-    
-    return op_camera_rows
+    if fieldname == 'Camera':
+        op_res = UFO_df[UFO_df[fieldname].str.contains(expr)]
+    elif fieldname == 'Pulse':
+        op_res = UFO_df[UFO_df[fieldname] == expr]
+    else:
+        print('Cannot find that field')
+        
+    return op_res
