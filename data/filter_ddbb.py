f3c65cd9d37c28996d0d48f5b7c2d94b6746ca62
Alejandro Gonzalez <agganzabal@gmail.com>
2024-02-15 16:57:45 +0100

    0.3 (AG): added function filter_camera_from_DDBB

--- data/filter_ddbb.py
+++ data/filter_ddbb.py
@@ -9,14 +9,17 @@ ChangeLog
 
     0.1 (AG): First version (GIT)
     0.2 (AG): Added pulse search; changed function arguments.
+    0.3 (AG): added filter_camera_from_DDBB. Changed the original's name
 """
 
 import pandas as pd
 
-def filter_camera_ddbb(fieldname, expr):
+def filter_expresion(fieldname, expr):
     """
     Filters the database found in complete_UFO_ddbb.xlsx and returns the cases 
     for the given camera passed as argument.
+
+    This is thought to be used in the completely unfiltered UFO DDBB from the VSO.
 
     Parameters
     ----------
@@ -28,7 +31,6 @@ def filter_camera_ddbb(fieldname, expr):
     -------
     op_camera_rows : dataframe
         The filtered pandas dataframe
-
     """
     
     ufo_path = "complete_UFO_ddbb.xlsx"
@@ -39,6 +41,30 @@ def filter_camera_ddbb(fieldname, expr):
     elif fieldname == 'Pulse':
         op_res = UFO_df[UFO_df[fieldname] == expr]
     else:
-        print('Cannot find that field')
+        raise IndexError('Cannot find that field')
         
     return op_res
+
+
+def filter_camera_from_DDBB(camera, ddbb):
+    """
+    Filters the data by the wanted camera.
+
+    Parameters
+    ---------
+    camera : str:
+        The name of the camera.
+    ddbb : str (path) or pd.Dataframe
+        the ddbb to evaluate.
+    """
+
+    if isinstance(ddbb, str):
+        df = pd.read_vsv(ddbb)
+    elif isinstance(ddbb, pd.DataFrame):
+        df = ddbb
+    else:
+        raise TypeError('Invalid type')
+
+    filtered_df = df[df['Exp.Cam'].str.contains(camera)]
+
+    return filtered_df
