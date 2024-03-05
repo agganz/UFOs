e277cfd5214de8ed43c5aea9d37478f650faf01f
agganz <jjb3129@heimdall136.jetdata.eu>
2024-03-05 15:09:33 +0000

    0.2 (AG): added get_video_from_row

--- data/get_video_data.py
+++ data/get_video_data.py
@@ -3,6 +3,7 @@
 #
 # Changelog:
 #       0.1 (AG): First version
+#       0.2 (AG): added get_video_from_row
 
 
 import sys
@@ -16,9 +17,20 @@ def get_videos(camera_name, pulse_id, time_detect):
     Downloads a video dataframe given the data needed.
     """
 
-    trange = (time_detect - 0.1, time_detect + 0.1)
+    trange = (time_detect - 0.2, time_detect + 0.2)
 
-    output_filename = camera_name + '_' + str(pulse_id)
+    output_filename = camera_name + '_' + str(pulse_id) + '.mp4'
     _ = jet2video.export_jet_video(camera_name, pulse_id, output_filename, fps = None, bitrate = 5000, dynamic_clim = True, clim = None, meta = ['jpn','camera','time'], time_range = trange, extract_bkg = True)
 
     return 1
+
+
+def get_video_from_row(df, row_number):
+    """
+    """
+
+    pulse_id = df['Pulse'][row_number]
+    camera_name = df['Exp.Cam'][row_number]
+    time_detect = df['Time'][row_number]
+
+    get_videos(camera_name, pulse_id, time_detect)
