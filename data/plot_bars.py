f6f7a4855b51ac4b545ec84187f5af23b10370d9
Alejandro Gonzalez <agganzabal@gmail.com>
2024-06-25 16:15:21 +0200

    NVC: updated data

--- data/plot_bars.py
+++ data/plot_bars.py
@@ -13,13 +13,13 @@ matplotlib.rc('font', **font)
 # other stands for Al + O
 
 material = ["Ti", "W", "Ni", "Mo", 'Other', "Combined", "NS/NC"] # before there was CU (0, 0)
-disr_materials = [18, 19, 0, 2, 0, 4, 13]
-no_disr_materials = [5, 2, 1, 1, 2, 0, 3]
+disr_materials = [18, 20, 0, 2, 0, 4, 13]
+no_disr_materials = [5, 3, 1, 3, 2, 0, 3]
 total_data_materials = list(np.array(disr_materials) + np.array(no_disr_materials))
 
 zone = ["UDPT", "IWGL", "UIWP", "NPL", "LH","ILA", "Divertor", "BEION", "4D", 'ICRH', 'NS/NC']
-no_disr_zone = [4, 0, 1, 0, 1, 0, 1, 1, 2, 1, 3]
-disr_zone = [15, 3, 3, 4, 0, 4, 13, 8, 4, 0, 2]
+no_disr_zone = [4, 0, 1, 0, 1, 0, 1, 4, 2, 1, 3]
+disr_zone = [15, 3, 3, 4, 0, 4, 13, 9, 4, 0, 2]
 total_data_zone = list(np.array(disr_zone) + np.array(no_disr_zone))
 
 print('Total data zone:', sum(total_data_zone))
