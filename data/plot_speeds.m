6b22655712cfb4d33541de2bcf238538fa2aa21f
Alejandro Gonzalez <agganzabal@gmail.com>
2024-07-03 16:11:41 +0200

    0.2 (AG): added crosses for negative data.

--- data/plot_speeds.m
+++ data/plot_speeds.m
@@ -2,25 +2,50 @@
 %
 % Changelog
 %   0.1 (AG): First version.
+%   0.2 (AG): added crosses for missing data.
 
 hold on
+disr_flag = false;
+no_disr_flag = false;
 
 for i = 1 : size(datavuv, 1)
     disr_time = datavuv.Disruption(i);
     speed = datavuv.MeasuredSpeed(i);
-    if datavuv.ExpCam == "KLDT-E5WD"
-        speed = speed / (322 * 416);
-    else
-        speed = speed / datavuv.Sc_Factor;
-    end
+    speed = speed / datavuv.Sc_factor(i);
 
     if disr_time == 0
-        bar(i, speed, 'FaceColor', "#2ca02C ")
+        if speed < 0
+            scatter(i, speed, 120, 'MarkerEdgeColor', '#2ca02C', 'LineWidth', 1, 'MarkerFaceColor', '#2ca02C', 'Marker', 'x', 'HandleVisibility', 'off')
+        else
+            if no_disr_flag
+                scatter(i, speed, 'MarkerFaceColor', "#2ca02C", 'MarkerEdgeColor','#2ca02C', 'HandleVisibility', 'off')
+            else
+                scatter(i, speed, 'MarkerFaceColor', "#2ca02C", 'MarkerEdgeColor','#2ca02C', 'DisplayName', 'Non-disruptive pulse')
+                no_disr_flag = true;
+            end
+        end
     else
-        bar(i, speed, 'FaceColor',"#d62728")
+        if speed < 0
+            scatter(i, speed, 120, 'MarkerFaceColor', '#d62728', 'LineWidth', 1, 'MarkerEdgeColor', '#d62728', 'Marker', 'x', 'HandleVisibility', 'off')
+        else
+            if disr_flag
+                scatter(i, speed, 'MarkerFaceColor', "#d62728", 'MarkerEdgeColor', '#d62728', 'HandleVisibility', 'off')
+            else
+                scatter(i, speed, 'MarkerFaceColor', "#d62728", 'MarkerEdgeColor', '#d62728', 'DisplayName', 'Disruptive pulse')
+                disr_flag = true;
+            end
+        end
     end
 end
 
+yline(0, 'k', 'HandleVisibility', 'off')
+
+title('Normalised TIE speeds', 'Interpreter', 'latex')
+xlabel('TIE in reduced dataset', 'Interpreter', 'latex')
+ylabel('Normalised TIE speed ($s^{-1} pix^{-1})$', 'Interpreter', 'latex')
+ax = gca;
+legend('Interpreter', 'latex')
+ax.FontSize = 15;
 box on
 grid on
 
