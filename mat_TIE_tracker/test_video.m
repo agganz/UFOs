6d356a94147c98f2e1d2921e8032d00f5db4df24
agonzalez <agonzalez@quasarsr.com>
2024-07-23 14:56:01 +0200

    TBT: NVC.

--- mat_TIE_tracker/test_video.m
+++ mat_TIE_tracker/test_video.m
@@ -23,29 +23,37 @@ while hasFrame(vid_obj)
     avg_image = mean(I_bw, 'all');
     [bboxes, scores] = detect(acfDetector, clean_image);
     box_idx = zeros(1, size(bboxes, 1));
+    boxes_int = zeros(1, size(bboxes, 1));
 
-    for i = 1 : size(bboxes, 1)
-        if scores(i) < 50
-            continue
-        end
-        mean_int_box = get_int_in_box(I_bw, bboxes(i, :));
-        if mean_int_box > avg_image + 0.25 * avg_image
-            box_idx(i) = i;
-        end
+    if n_frame == 37
+        beep
     end
 
-    box_idx = box_idx(box_idx > 0);
+    for i = 1 : size(bboxes, 1)
+        mean_int_box = get_int_in_box(I_bw, bboxes(i, :));
+        boxes_int(i) = mean_int_box;
+    end
+
+    prod_int_scores = boxes_int .* scores';
+    box_idx = prod_int_scores > 700;
+%     box_idx = box_idx(box_idx > 0);
+
     bboxes = bboxes(box_idx, :);
     scores = scores(box_idx);
-    [~, scr_idx] = maxk(scores, 3);
-    bboxes = bboxes(scr_idx, :);
-    scores = scores(scr_idx);
+%     [~, scr_idx] = maxk(scores, 3);
+%     bboxes = bboxes(scr_idx, :);
+%     scores = scores(scr_idx);
 
+    prods_to_use = prod_int_scores(box_idx);
+    
+    if isempty(scores)
+        continue
+    end
 
     annotation = acfDetector.ModelName;
     % I = insertObjectAnnotation(I,'rectangle',bboxes,annotation);
     I_clean = insertText(I_bw, [2, 2], num2str(n_frame));
-    I_clean = insertObjectAnnotation(I_clean,'rectangle',bboxes, scores);
+    I_clean = insertObjectAnnotation(I_clean,'rectangle',bboxes, prods_to_use);
     
     writeVideo(vid_out, I_clean)
 end
