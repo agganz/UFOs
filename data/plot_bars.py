12ec1745e80a648b3b6e52bea6ad5ac053057746
Alejandro Gonzalez <agganzabal@gmail.com>
2024-08-05 15:28:26 +0200

    0.2.1 (AG): added TIEs.

--- data/plot_bars.py
+++ data/plot_bars.py
@@ -4,6 +4,7 @@
 # Changelog:
 #    0.1 (AG): first version.
 #    0.2 (AG): fixed label in zones.
+#    0.2.1 (AG): changed the quantity of items.
 
 import matplotlib
 from matplotlib import pyplot as plt
@@ -18,20 +19,20 @@ matplotlib.rc('font', **font)
 
 # other stands for Al + O
 
-material = ["Ti", "W", "Ni", "Mo", 'Other', "Combined", "NS/NC"] # before there was CU (0, 0)
+material = ["Ti", "W", "Ni", "Mo", 'Other', "Comb.", "NS/NC"] # before there was CU (0, 0)
 disr_materials = [18, 20, 0, 2, 0, 4, 13]
-no_disr_materials = [5, 3, 1, 3, 2, 0, 3]
+no_disr_materials = [8, 3, 1, 3, 2, 0, 3]
 total_data_materials = list(np.array(disr_materials) + np.array(no_disr_materials))
 
 zone = ["UDPT", "IWGL", "UIWP", "NPL", "LH","ILA", "Divertor", "BEION4", "4D", 'ICRH', 'NS/NC']
-no_disr_zone = [4, 0, 1, 0, 1, 0, 1, 4, 2, 1, 3]
+no_disr_zone = [6, 0, 1, 0, 1, 0, 1, 5, 2, 1, 3]
 disr_zone = [15, 3, 3, 4, 0, 4, 13, 9, 4, 0, 2]
 total_data_zone = list(np.array(disr_zone) + np.array(no_disr_zone))
 
 print('Total data zone:', sum(total_data_zone))
 print('Total data materials:', sum(total_data_materials))
 
-def plot_bar(disr, no_disr, labels):
+def plot_bar(disr, no_disr, labels, x_label, title_plot):
     """
     Plot a bar chart with two sets of data.
     """
@@ -43,13 +44,13 @@ def plot_bar(disr, no_disr, labels):
     new_y_axis = range(math.floor(min(total_elems)), math.ceil(max(total_elems))+1)
 
     fig, ax = plt.subplots()
-    fig.set_size_inches(10, 8)
+    fig.set_size_inches(9, 8)
     rects1 = ax.bar(x - width/2, disr, width, label='Disruptive', color='tab:red')
     rects2 = ax.bar(x + width/2, no_disr, width, label='Non Disruptive', color='tab:green')
 
     ax.set_ylabel('Number of TIEs')
-    ax.set_xlabel('Area in device')
-    ax.set_title('Location of TIEs in disruptive and non-disruptive discharges')
+    ax.set_xlabel(x_label)
+    ax.set_title(title_plot)
     ax.set_xticks(x)
     ax.set_xticklabels(labels)
     ax.set_yticks(new_y_axis)
@@ -59,4 +60,10 @@ def plot_bar(disr, no_disr, labels):
     plt.show()
 
 # Example usage
-plot_bar(disr_zone, no_disr_zone, zone)
+titlemat = 'TIE composition in disruptive and non-disruptive discharges'
+xlabelmat = 'TIE composition'
+
+titlezone ='Location of TIEs in disruptive and non-disruptive discharges'
+xlabelzone = 'Area in device'
+
+plot_bar(disr_materials, no_disr_materials, material, xlabelmat, titlemat)
