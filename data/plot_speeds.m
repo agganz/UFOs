02a49401d02b458d739e5de1ccd0570ba44ab0ec
Alejandro Gonzalez <agganzabal@gmail.com>
2024-08-07 15:38:20 +0200

    0.2.2 (AG): added more detail to x legend.

--- data/plot_speeds.m
+++ data/plot_speeds.m
@@ -4,10 +4,13 @@
 %   0.1 (AG): First version.
 %   0.2 (AG): added crosses for missing data.
 %   0.2.1 (AG): changed the column of the scfactor.
+%   0.2.2 (AG): added more detail to the x legend.
 
 hold on
 disr_flag = false;
 no_disr_flag = false;
+disr_no_mes_flag = false;
+no_disr_no_mes_flag = false;
 
 for i = 1 : size(datavuv, 1)
     disr_time = datavuv.Disruption(i);
@@ -16,18 +19,31 @@ for i = 1 : size(datavuv, 1)
 
     if disr_time == 0
         if speed < 0
-            scatter(i, speed, 120, 'MarkerEdgeColor', '#2ca02C', 'LineWidth', 1, 'MarkerFaceColor', '#2ca02C', 'Marker', 'x', 'HandleVisibility', 'off')
+            speed = -10;
+            if no_disr_no_mes_flag
+                scatter(i, speed, 120, 'MarkerEdgeColor', "#0072BD", 'LineWidth', 1, 'MarkerFaceColor', '#2ca02C', 'Marker', 'x', 'HandleVisibility', 'off')
+            else
+                scatter(i, speed, 120, 'MarkerEdgeColor', "#0072BD", 'LineWidth', 1, 'MarkerFaceColor', '#2ca02C', 'Marker', 'x', 'DisplayName', "Non-Disruptive, can't measure")
+                no_disr_no_mes_flag = true;
+            end
         else
             if no_disr_flag
-                scatter(i, speed, 'MarkerFaceColor', "#2ca02C", 'MarkerEdgeColor','#2ca02C', 'HandleVisibility', 'off')
+                scatter(i, speed, 'MarkerFaceColor', "#0072BD", 'MarkerEdgeColor','#2ca02C', 'HandleVisibility', 'off')
             else
-                scatter(i, speed, 'MarkerFaceColor', "#2ca02C", 'MarkerEdgeColor','#2ca02C', 'DisplayName', 'Non-disruptive pulse')
+                scatter(i, speed, 'MarkerFaceColor', "#0072BD", 'MarkerEdgeColor','#2ca02C', 'DisplayName', 'Non-disruptive pulse')
                 no_disr_flag = true;
             end
         end
     else
         if speed < 0
-            scatter(i, speed, 120, 'MarkerFaceColor', '#d62728', 'LineWidth', 1, 'MarkerEdgeColor', '#d62728', 'Marker', 'x', 'HandleVisibility', 'off')
+            speed = -10;
+            if disr_no_mes_flag
+                scatter(i, speed, 120, 'MarkerFaceColor', '#d62728', 'LineWidth', 1, 'MarkerEdgeColor', '#d62728', 'Marker', 'x', 'HandleVisibility', 'off')
+            else
+                scatter(i, speed, 120, 'MarkerFaceColor', '#d62728', 'LineWidth', 1, 'MarkerEdgeColor', '#d62728', 'Marker', 'x', 'DisplayName', "Disruptive, can't measure")
+
+                disr_no_mes_flag = true;
+            end
         else
             if disr_flag
                 scatter(i, speed, 'MarkerFaceColor', "#d62728", 'MarkerEdgeColor', '#d62728', 'HandleVisibility', 'off')
