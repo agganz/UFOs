9291e24fcab4938e195d817e9927579b803763af
Alejandro Gonzalez <agganzabal@gmail.com>
2024-08-07 15:39:27 +0200

    NVC: data updated.

--- old/plot_pies.py
+++ old/plot_pies.py
@@ -14,15 +14,19 @@ matplotlib.rc('font', **font)
 
 # other stands for Al + O
 
-material = ["Ti", "W", "Cu", "Ni", "Mo", 'Other', "Combined", "Undefined"]
-disr_materials = [18, 19, 0, 0, 2, 0, 4, 13]
-no_disr_materials = [5, 2, 0, 1, 1, 2, 0, 3]
+material = ["Ti", "W", "Ni", "Mo", 'Other', "Comb.", "NS/NC"] # before there was CU (0, 0)
+disr_materials = [18, 20, 0, 2, 0, 4, 13]
+no_disr_materials = [8, 3, 1, 3, 2, 0, 3]
 total_data_materials = list(np.array(disr_materials) + np.array(no_disr_materials))
 
-zone = ["UDPT", "IWGL", "UIWP", "NPL", "LH","ILA", "Divertor", "BEION", "4D", 'ICRH', 'NA']
-no_disr_zone = [4, 0, 1, 0, 1, 0, 1, 1, 2, 1, 3]
-disr_zone = [14, 3, 3, 4, 0, 4, 13, 8, 4, 0, 3]
+zone = ["UDPT", "IWGL", "UIWP", "NPL", "LH","ILA", "Divertor", "BEION4", "4D", 'ICRH', 'NS/NC']
+no_disr_zone = [6, 0, 1, 0, 1, 0, 1, 5, 2, 1, 3]
+disr_zone = [15, 3, 3, 4, 0, 4, 13, 9, 4, 0, 2]
 total_data_zone = list(np.array(disr_zone) + np.array(no_disr_zone))
+
+print('Total data zone:', sum(total_data_zone))
+print('Total data materials:', sum(total_data_materials))
+
 
 def plot_pie_with_legend(labels, values, add_legend = True, cus_title = None, fig_name = 'demo', cut_off_legend = False):
     """
