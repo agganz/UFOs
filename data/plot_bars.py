e24284b3f55bfd3529175b9150d8005042b6bb35
Alejandro Gonzalez <agganzabal@gmail.com>
2024-09-13 15:32:01 +0200

    0.2.2 (AG): shortcut for plots.

--- data/plot_bars.py
+++ data/plot_bars.py
@@ -5,6 +5,7 @@
 #    0.1 (AG): first version.
 #    0.2 (AG): fixed label in zones.
 #    0.2.1 (AG): changed the quantity of items.
+#    0.2.2 (AG): added mode
 
 import matplotlib
 from matplotlib import pyplot as plt
@@ -19,7 +20,7 @@ matplotlib.rc('font', **font)
 
 # other stands for Al + O
 
-material = ["Ti", "W", "Ni", "Mo", 'Other', "Comb.", "NS/NC"] # before there was CU (0, 0)
+material = ["Ti", "W", "Ni", "Mo", 'Other', "Combined", "NS/NC"] # before there was CU (0, 0)
 disr_materials = [18, 20, 0, 2, 0, 4, 13]
 no_disr_materials = [8, 3, 1, 3, 2, 0, 3]
 total_data_materials = list(np.array(disr_materials) + np.array(no_disr_materials))
@@ -66,4 +67,8 @@ xlabelmat = 'TIE composition'
 titlezone ='Location of TIEs in disruptive and non-disruptive discharges'
 xlabelzone = 'Area in device'
 
-plot_bar(disr_zone, no_disr_zone, zone, xlabelzone, titlezone)
+mode = 'material'
+if mode == 'material':
+    plot_bar(disr_materials, no_disr_materials, material, xlabelmat, titlemat)
+elif mode == 'zone':
+    plot_bar(disr_zone, no_disr_zone, zone, xlabelzone, titlezone)
